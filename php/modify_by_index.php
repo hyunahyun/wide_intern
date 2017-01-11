8416396f3e2f331b90f0b8ff916b7d201ac1eb95
hyunahyun <tn3125977@gmail.com>
2017-01-11 12:25:48 +0900

    3차 UI 완성

--- php/modify_by_index.php
+++ php/modify_by_index.php
@@ -28,22 +28,12 @@ if(!strcmp($table,"tb_motionid")){
 else if(!strcmp($table,"tb_category")){
 	$value_arr = split(";",$values);
 	
-	//카테고리 데이터 존재 여부 체크
-	$query = "select seq from tb_motionid where motion_type = (select cate_name from tb_category where seq = '$index');";
-	$result = $connect->query($query);
-	
-	if($row = mysqli_fetch_row($result)){ // 있으면
-		//tb_motionid 수정 - 카테고리 이름
-		$query = "update tb_motionid set motion_type='$value_arr[0]' where motion_type = (select cate_name from tb_category where seq = '$index');";
-		$connect->query($query);
+	//모션 아이디 DB에서 카테고리 수정
+	$query = "update tb_motionid set motion_type='$value_arr[0]' where motion_type = (select cate_name from tb_category where seq = '$index');";
+	$connect->query($query);
 
-		// tb_category 수정
-		$query = "update tb_category set cate_name='$value_arr[0]', cate_params='$value_arr[2]' where seq='$index';";
-	}
-	else{ // 없으면
-		// tb_category 수정
-		$query = "update tb_category set cate_name='$value_arr[0]', cate_serial='$value_arr[1]', cate_params='$value_arr[2]' where seq='$index';";
-	}
+	//카테고리 DB에서 카테고리 수정
+	$query = "update tb_category set cate_name='$value_arr[0]', cate_serial='$value_arr[1]', cate_params='$value_arr[2]' where seq='$index';";
 }
 
 $connect->query($query);
