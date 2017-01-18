ed70f55669128c33affea4a958673605005bf821
hyunahyun <tn3125977@gmail.com>
2017-01-18 15:18:33 +0900

    중복되는 카테고리 알람창 수정

--- php/make_cate.php
+++ php/make_cate.php
@@ -15,10 +15,16 @@ $check = mysqli_fetch_row($result);
 if($check[0] == null) $num = 1;
 else $num = $check[0] + 1;
 
-$check_query = "select seq from tb_category where cate_name='$name' or cate_serial='$serial';";
-if(!($temp = $connect->query($check_query))) throw new Exception($connect->error);
+$check_query1 = "select seq from tb_category where cate_name='$name';";
+if(!($temp1 = $connect->query($check_query1))) throw new Exception($connect->error);
 
-if($temp->num_rows == 0){ // 겹치지 않을 경우
+$check_query2 = "select seq from tb_category where cate_serial='$serial';";
+if(!($temp2 = $connect->query($check_query2))) throw new Exception($connect->error);
+
+if(($temp1->num_rows != 0) && ($temp2->num_rows != 0)) echo "기존의 카테고리와 이름과 고유키가 동일합니다.";
+else if($temp1->num_rows != 0) echo "기존의 카테고리와 이름이 동일합니다.";
+else if($temp2->num_rows != 0) echo "기존의 카테고리와 고유키가 동일합니다.";
+else{ // 겹치지 않을 경우
 	$param_arr = split(",",$params);
 	$param_value = split(",",$values);
 	
@@ -44,7 +50,6 @@ if($temp->num_rows == 0){ // 겹치지 않을 경우
 
 	if(!$connect->query($query)) throw new Exception($connect->error); 
 }
-else echo "can't adding!";
  
 ?>
  
