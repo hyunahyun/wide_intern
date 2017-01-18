c2c237bb3d696eb6ee5180901a9e2a4cb4781426
hyunahyun <tn3125977@gmail.com>
2017-01-18 11:52:55 +0900

    카테고리 추가 에러 수정

--- php/make_cate.php
+++ php/make_cate.php
@@ -18,37 +18,33 @@ else $num = $check[0] + 1;
 $check_query = "select seq from tb_category where cate_name='$name' or cate_serial='$serial';";
 if(!($temp = $connect->query($check_query))) throw new Exception($connect->error);
 
-$row = mysqli_fetch_row($temp);
-
-if(!$row){ // 겹치지 않을 경우
-	
-	//카테고리 추가
-	$query = "insert into tb_category (seq, cate_name, cate_serial, cate_params) values ('$num', '$name','$serial','$params');";
-	if(!$connect->query($query)) throw new Exception($connect->error); 
-
+if($temp->num_rows == 0){ // 겹치지 않을 경우
 	$param_arr = split(",",$params);
 	$param_value = split(",",$values);
 	
-	//필요 파라미터 업데이트
-	$query = "update tb_category set ";
-		
-	for($i=0;$i< sizeof($param_arr);$i++){
-		if($i < (sizeof($param_arr)-1) ){ 
-			$temp = $param_arr[$i]."='$param_value[$i]', ";
-			$query .= $temp;
+	//카테고리 추가
+	$query = "insert into tb_category (seq, cate_name, cate_serial, cate_params";
+	
+	//필요 파라미터 추가
+	if($param_arr[0] != ""){
+		for($i=0;$i< sizeof($param_arr);$i++){
+			$query .= ", $param_arr[$i]";
 		}
-		else{
-			$temp = "$param_arr[$i]='$param_value[$i]'";
-			$query .= $temp;
+	}
+	
+	$query .=	") values ('$num', '$name', '$serial', '$params'";
+	
+	if($param_value[0] != ""){
+		for($i=0;$i< sizeof($param_value);$i++){
+			$query .= ", '$param_value[$i]'";
 		}
 	}
 		
-	$query .=	" where seq='$num';";
+	$query .=	");";
 
 	if(!$connect->query($query)) throw new Exception($connect->error); 
 }
 else echo "can't adding!";
- 
  
 ?>
  
